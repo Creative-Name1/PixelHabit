41c6bd289d66cca0b2f98e17a96c3a87d16e7ee0
Bogong <ipopomusicworks@gmail.com>
2026-02-07 12:24:04 -0500

    Mostly finished default habit layout. Needs some tweaks later

--- app/src/main/java/com/example/pixelhabit_attempt3/MainActivity.java
+++ app/src/main/java/com/example/pixelhabit_attempt3/MainActivity.java
@@ -7,16 +7,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // opens the activity_main.xml layout
+        setContentView(R.layout.habit); // opens the activity_main.xml layout
 
         Button button = findViewById(R.id.button1);
         EditText editText1 = findViewById(R.id.editText1);
