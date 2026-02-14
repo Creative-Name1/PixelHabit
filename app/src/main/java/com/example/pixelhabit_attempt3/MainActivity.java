b4736fc8b57583fef23f72273966564064370ddc
Bogong <ipopomusicworks@gmail.com>
2026-02-14 17:31:51 -0500

    Added File I/O

--- app/src/main/java/com/example/pixelhabit_attempt3/MainActivity.java
+++ app/src/main/java/com/example/pixelhabit_attempt3/MainActivity.java
@@ -14,50 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ArrayList<Habit> habitList = new ArrayList<Habit>();
+        StorageManager s = new StorageManager(this);
 
-        // [TESTING JUNK]
-        habitList.add(new Habit());
-        habitList.add(new Habit("Cool habit name", "this is a description"));
-        habitList.add(new Habit("Meditate twice daily", "Who could possibly set this habit", 1, 0, 2, 12));
-        habitList.add(new Habit());
-        habitList.add(new Habit("Eat 4-5 dozen eggs a day", "(No description)"));
-        habitList.add(new Habit());
-        habitList.add(new Habit());
-        habitList.add(new Habit("Make sure to drink 6-7 glasses of water a day to stay tuff", "(no description)", 1, 0, 6, 7));
-        habitList.add(new Habit("Run a 15k every morning", "or at least 7k"));
-        habitList.add(new Habit());
-        habitList.get(1).setStreak(2);
-        habitList.get(2).setStreakPositive(false);
-        habitList.get(2).setStreak(4);
-        habitList.get(4).setStreak(7);
-        habitList.get(5).setStreakPositive(false);
-        habitList.get(5).setStreak(1);
-        habitList.get(6).setStreakPositive(false);
-        habitList.get(6).setStreak(2);
-        habitList.get(7).setStreak(17);
-        habitList.get(8).setStreakPositive(false);
-        habitList.get(8).setStreak(2147483647);
-        // [TESTING JUNK]
+        ArrayList<Habit> completeHabitList = s.getCompleteHabitList();
 
+//        completeHabitList.add(new Habit());
+//        completeHabitList.add(new Habit("Cool habit name", "this is a description"));
+//        completeHabitList.add(new Habit("Meditate twice daily", "Who could possibly set this habit", 1, 0, 2, 12));
+//        completeHabitList.add(new Habit());
+//        completeHabitList.add(new Habit("Eat 4-5 dozen eggs a day", "(No description)"));
+//        completeHabitList.add(new Habit());
+//        completeHabitList.add(new Habit());
+//
+//        completeHabitList.add(new Habit("Make sure to drink 6-7 glasses of water a day to stay tuff", "(no description)", 1, 0, 6, 7));
 
+        s.saveHabitList(new ArrayList<Habit>(), completeHabitList);
 
-
-
-
-        // [HANDLE INTENT FROM EDITHABIT ACTIVITY]
-        Intent editHabitData = getIntent();
-        if (editHabitData.getBooleanExtra("edits_were_made", false)) {
-            Habit h = habitList.get(editHabitData.getIntExtra("index", 0));
-
-            h.setName(editHabitData.getStringExtra("name"));
-            h.setDescription(editHabitData.getStringExtra("description"));
-        }
-        // [HANDLE INTENT FROM EDITHABIT ACTIVITY]
-
-
-
-        Habit[] habitArray = habitList.toArray(new Habit[0]);
+        Habit[] habitArray = completeHabitList.toArray(new Habit[0]);
 
         setContentView(R.layout.habit_list);
 
@@ -66,5 +39,43 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setAdapter(listAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+//        ArrayList<Habit> habitList = new ArrayList<Habit>();
+//
+//        // [TESTING JUNK]
+//
+//        habitList.add(new Habit("Make sure to drink 6-7 glasses of water a day to stay tuff", "(no description)", 1, 0, 6, 7));
+//        habitList.add(new Habit("Run a 15k every morning", "or at least 7k"));
+//        habitList.add(new Habit());
+//        habitList.get(1).setStreak(2);
+//        habitList.get(2).setStreakPositive(false);
+//        habitList.get(2).setStreak(4);
+//        habitList.get(4).setStreak(7);
+//        habitList.get(5).setStreakPositive(false);
+//        habitList.get(5).setStreak(1);
+//        habitList.get(6).setStreakPositive(false);
+//        habitList.get(6).setStreak(2);
+//        habitList.get(7).setStreak(17);
+//        habitList.get(8).setStreakPositive(false);
+//        habitList.get(8).setStreak(2147483647);
+//        // [TESTING JUNK]
+//
+//
+//
+//
+//
+//
+//        // [HANDLE INTENT FROM EDITHABIT ACTIVITY]
+//        Intent editHabitData = getIntent();
+//        if (editHabitData.getBooleanExtra("edits_were_made", false)) {
+//            Habit h = habitList.get(editHabitData.getIntExtra("index", 0));
+//
+//            h.setName(editHabitData.getStringExtra("name"));
+//            h.setDescription(editHabitData.getStringExtra("description"));
+//        }
+//        // [HANDLE INTENT FROM EDITHABIT ACTIVITY]
+//
+//
+//
     }
 }
--- app/src/main/java/com/example/pixelhabit_attempt3/StorageManager.java
+++ app/src/main/java/com/example/pixelhabit_attempt3/StorageManager.java
@@ -4,21 +4,69 @@ import static android.content.Context.MODE_PRIVATE;
 import android.content.Context;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileOutputStream; // using this instead of FileWriter for compatibility
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class StorageManager {
-    public void saveHabitList(ArrayList<Habit> list, Context context) {
-        try {
-            FileOutputStream writer = context.openFileOutput("habit_list", MODE_PRIVATE);
+    Context context; // this will be whatever Activity made this
 
+    public StorageManager(Context c) {
+        context = c;
+    }
+
+    public void saveHabitList(ArrayList<Habit> toDoList, ArrayList<Habit> completeList) {
+        try {
+            Gson jsonConverter = new Gson();
+
+            FileOutputStream fosIncomplete = context.openFileOutput("habit_list_incomplete.json", MODE_PRIVATE);
+            PrintWriter pwIncomplete = new PrintWriter(fosIncomplete);
+
+            FileOutputStream fosComplete = context.openFileOutput("habit_list_complete.json", MODE_PRIVATE);
+            PrintWriter pwComplete = new PrintWriter(fosComplete);
+
+            pwIncomplete.write(jsonConverter.toJson(toDoList));
+            pwComplete.write(jsonConverter.toJson(completeList));
+
+            pwComplete.close();
+            pwIncomplete.close();
         } catch (IOException e) {
             Log.d("storage_manager", ("Save failed: " + e.getMessage()), e);
         }
-
     }
-    public ArrayList<Habit> getHabitList() {
-        return new ArrayList<Habit>();
+
+    public ArrayList<Habit> getCompleteHabitList() {
+        try {
+            Gson jsonInterpreter = new Gson();
+
+            FileInputStream fis = context.openFileInput("habit_list_complete.json");
+            BufferedReader jsonReader = new BufferedReader(new InputStreamReader(fis)); // inputstreamreader converts bytes to characters, bufferedreader converts characters to strings
+
+            String outJson = "";
+
+            // BufferedReader calls readline, keeps track of index on its own, returns null when the line DNE
+            for (String outAdd = jsonReader.readLine(); // when initializing, we make a string outadd equal to the first line
+                 outAdd != null; // bufferedreader returns null when there are no lines left to read
+                 outAdd = jsonReader.readLine()) { // ofc we set outadd to the next line each repeat
+
+                outJson += outAdd;
+            }
+
+            Type habitList = new TypeToken<ArrayList<Habit>>(){}.getType();
+            return new ArrayList<Habit>(jsonInterpreter.fromJson(outJson, habitList));
+        } catch (IOException e) {
+            Log.d("storage_manager", ("Load failed: " + e.getMessage()), e);
+            return new ArrayList<Habit>();
+        }
     }
 }
